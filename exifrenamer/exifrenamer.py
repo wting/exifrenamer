daa84bcbe274b5c630d147636dcc7ac9c87c66c2
William Ting <io@williamting.com>
2013-12-30 11:05:09 -0600

    linter corrections

--- exifrenamer/exifrenamer.py
+++ exifrenamer/exifrenamer.py
@@ -3,19 +3,19 @@
 """
   Copyright © 2009-2013 William Ting
 
-  *  This program is free software; you can redistribute it and/or modify
+  * This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 3, or (at your option)
   any later version.
 
-  *  This program is distributed in the hope that it will be useful,
+  * This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
-  *  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  * You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 """
 
 
@@ -170,7 +170,8 @@ def parse_args():
         sys.exit(1)
 
     if args.input_dir == args.output_dir:
-        sys.stderr.write("Input and output directories need to be different.\n")
+        sys.stderr.write("Input and output directories need to be \
+                different.\n")
         sys.exit(1)
 
     if args.simulate:
