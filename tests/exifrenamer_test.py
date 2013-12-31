90c489060670e0e9619a446be15657a37b3cbb81
William Ting <io@williamting.com>
2013-12-31 12:22:35 -0600

    add simple filename alternate test

--- tests/exifrenamer_test.py
+++ tests/exifrenamer_test.py
@@ -3,17 +3,17 @@
 
 from testify import *
 
+from exifrenamer import find_alternate_filename
 from exifrenamer import increment
 
 
 class HelpersTestCase(TestCase):
 
-    @class_setup
-    def init(self):
-        self.var = 0
-
     def test_increment(self):
         assert_equal(increment(0), 1)
+
+    def test_good_fan_filenames(self):
+        assert_equal(find_alternate_filename('foo.jpg'), 'foo-0000.jpg')
 
 
 if __name__ == "__main__":
